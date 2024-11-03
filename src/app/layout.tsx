517bbc35a43b734f2a385f3d1a7cf0e5337d0251
Pezhman <33788011+priestmoon50@users.noreply.github.com>
2024-11-03 19:54:14 +0330

    Last Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,8 @@
 // src/app/layout.tsx
 
-import localFont from 'next/font/local';
 import './globals.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-const iranianSansFont = localFont({
-  src: '/fonts/IranianSans.ttf',
-  variable: '--font-iranian-sans',
-  display: 'swap', // استفاده از گزینه swap برای بهبود تجربه کاربری در زمان بارگذاری
-});
 
 export default function RootLayout({
   children,
@@ -17,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fa" className={iranianSansFont.variable}>
+    <html lang="fa">
       <body>
         <Header />
         <main>{children}</main>
